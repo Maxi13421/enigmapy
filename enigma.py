01ed6bfd5cece3e646b557c521619910db2b6661
Selemba <60011459+Selemba1000@users.noreply.github.com>
2022-03-07 13:42:17 +0100

    Walze 1

--- enigma.py
+++ enigma.py
@@ -8,4 +8,4 @@ class Enigma:
 class Walze:
   def __init__(self,nummer):
     if(nummer=='1'):
-      self.verdrahtung=[5,11,]
+      self.verdrahtung=[5,11,13,6,12,7,4,17,22,26,14,20,15,23,25,8,24,21,19,16,1,9,2,18,3,10]
